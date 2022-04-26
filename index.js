330a749b15b6a07ca4b136a9ad06e95adc6613da
Ignacio Yz <ignacioyz@gmail.com>
2022-04-25 22:03:06 -0400

    commit tree e947d0f9d990537bc32b73a4f9e90b87e31dba38

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31 
+const num2 = 2 
+const multiply = num1 * num2 
 
-// Write your code here
+const num3 = 24 
+const random = num1 * num3 
+
+const num4 = 10
+const mod = num3 % num4
+
+const max = Math.max(12, 15, 20)
